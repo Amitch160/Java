be1d10954a059cac9c1d3a55dcb2d3bdc2bfb6ab
Amit kumar <110763284+Amitch160@users.noreply.github.com>
2022-09-02 00:03:27 +0530

    Add files via upload

---
+++ input_in_java.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class input_in_java {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        // String name = sc.next();
+        // String name = sc.nextLine();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+        // nextInt
+        // nextFloat
+
+        System.out.println(name);
+    }
+    
+}
